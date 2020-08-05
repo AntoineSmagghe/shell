75b80fdb3d813ac5b9c9644d7533dd52bca7783b
Antoine Smagghe <antoine@smagghe.me>
2020-08-05 12:32:03 +0200

    add batch test to create logo html page

---
+++ windows/logo.bat
@@ -0,0 +1,4 @@
+\<style\>#logos{display:flex;flex-wrap:wrap;width:1113px;margin:auto;justify-content:center}.logo{width:228px;object-fit:scale-down;max-height:110px;padding:15px;display:flex;margin:auto 0}\<\/style\> > test.html
+\<section id='logos'\> >> test.html
+for %%a in (*.ink) do echo \<img src='%%a' class='logo'\> >> test.html
+\<\/section\> >> test.html
