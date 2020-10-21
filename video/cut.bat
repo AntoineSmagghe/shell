2a087931d7504ecf2cc46198dac8223c17d0f45d
Antoine SMAGGHE <SMAGGHE@PROXIMUM.proximum.fr>
2020-10-21 10:29:10 +0200

    minor configurations in variables for cut.bat

--- video/cut.bat
+++ video/cut.bat
@@ -1,22 +1,22 @@
 @echo off
-echo **********************************************************
-echo *                                                        *
-echo *  Bienvenue dans l'utilitaire de decoupe video Proximum *
-echo *                                                        *
-echo **********************************************************
+echo ***********************************************************
+echo *                                                         *
+echo *  Bienvenue dans l'utilitaire de decoupe video Proximum  *
+echo *                                                         *
+echo ***********************************************************
 
 set inputFile=%1
 set start=%2
 set end=%3
 set outputFile=%4
 
-set "x = 0" 
+set "x=0" 
 
 FOR %%A IN (%*) DO (
     set /a "x+=1"
-    IF "%%A" == "-help" ECHO nice try !
+    IF "%%A" == "-help" ECHO non !
 )
 
 IF %x% GTR 2 (ffmpeg.exe -t %end% -i %inputFile% -ss %start% -vcodec libx264 -acodec copy -preset ultrafast -filter:v fps=fps=25 -b:v 800k %outputFile%)
 IF %x% EQU 2 (ffmpeg.exe -i %inputFile% -vcodec libx264 -acodec copy -preset ultrafast -filter:v fps=fps=25 -b 800k %start%)
-IF %x% EQU 0 (echo Veuillez entrer une commande pour convertir ou couper les vidéos.) ELSE (echo Erreur dans les parametres)
+IF %x% EQU "" (echo Veuillez entrer une commande pour convertir ou couper les vidéos.) ELSE (echo Erreur: pas assez dans les parametres)
