e0115d72afc8e530cfe3ec7d2661206d7f9abdc4
Antoine SMAGGHE <SMAGGHE@PROXIMUM.proximum.fr>
2020-10-21 12:33:21 +0200

    Change cut.bat to multiple choices options

--- video/cut.bat
+++ video/cut.bat
@@ -1,22 +1,62 @@
 @echo off
+color 04
 echo ***********************************************************
 echo *                                                         *
 echo *  Bienvenue dans l'utilitaire de decoupe video Proximum  *
+echo *                   version 1.0.0                         *
 echo *                                                         *
 echo ***********************************************************
+echo *
+echo * A tout moment tappez ctrl+c pour sortir du programme
+echo * 
+echo *  Si vous souhaitez juste convertir une video sans la couper,
+echo *  Passez les questions de debut et de fin de la video en 
+echo *  cliquant sur "Entrer"
+echo *  
+echo *  Bonne chance!
+echo *
 
-set inputFile=%1
-set start=%2
-set end=%3
-set outputFile=%4
+set /p inputFile=* Glissez et deposer la video a traiter ici
+echo * La video a traiter sera %inputFile%
+echo *
+echo ************************************************************
+echo *
+set /p start=* Inscrire le debut de la video au format (HH:MM:SS)
+echo * La video commencera a %start%
+echo * 
+echo ************************************************************
+echo *
+set /p end=* Inscrire la fin de la video au format (HH:MM:SS)
+echo * La video terminera a %end%
+echo *
+echo ************************************************************
 
-set "x=0" 
-
-FOR %%A IN (%*) DO (
-    set /a "x+=1"
-    IF "%%A" == "-help" ECHO non !
+FOR %%A IN (%*%) DO (
+    IF "%%A"=="q" EXIT
 )
 
-IF %x% GTR 2 (ffmpeg.exe -t %end% -i %inputFile% -ss %start% -vcodec libx264 -acodec copy -preset ultrafast -filter:v fps=fps=25 -b:v 800k %outputFile%)
-IF %x% EQU 2 (ffmpeg.exe -i %inputFile% -vcodec libx264 -acodec copy -preset ultrafast -filter:v fps=fps=25 -b 800k %start%)
-IF %x% EQU "" (echo Veuillez entrer une commande pour convertir ou couper les vidéos.) ELSE (echo Erreur: pas assez dans les parametres)
+IF "%inputFile%"=="" GOTO :parametresError
+set outputFile=%inputFile:~0,-4%-convert.mp4
+
+IF "%start%"=="" IF "%end%"=="" GOTO :simpleConversion
+IF "%start%"=="" GOTO :parametresError
+IF "%end%"=="" GOTO :parametresError
+
+ffmpeg.exe -t %end% -i %inputFile% -ss %start% -vcodec libx264 -acodec copy -preset ultrafast -filter:v fps=fps=25 -b:v 800k %outputFile%
+EXIT
+
+:simpleConversion
+ffmpeg.exe -i %inputFile% -vcodec libx264 -acodec copy -preset ultrafast -filter:v fps=fps=25 -b 800k %outputFile%
+EXIT
+
+:parametresError
+echo *
+echo * Erreur: veuillez entrer entrer de nouveau 
+echo * les commandes pour convertir ou couper une video.
+
+:aurevoir
+echo * 
+echo * Au revoir !
+echo * 
+pause
+EXIT
