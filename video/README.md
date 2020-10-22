ccc2c0201d50e6e2d080ee522bbab3ca1e005992
Antoine SMAGGHE <SMAGGHE@PROXIMUM.proximum.fr>
2020-10-22 10:03:32 +0200

    NEW: readme information to convert video

--- video/README.md
+++ video/README.md
@@ -1,3 +1,13 @@
-TUTORIEL 
+TUTORIEL POUR DECOUPER OU COUVERTIR UNE VIDEO
+SI VOUS SOUHAITEZ UNIQUEMENT CONVERTIR LA VIDEO PASSER A L'ETAPE # 3
 
-1 # Ouvrez le fichier avec 
+
+# 1 Ouvrez le fichier vidéo avec votre lecteur préféré.
+
+# 2 Repérer le début et la fin de la vidéo au format "HH:MM:SS" ou "MM:SS" ou "SS"
+
+# 3 Ouvrez le programme nommé "video-converter.bat" ou "video-converter"
+
+# 4 Suivez les instructions, appuyez à chaque fois sur "Entrer" pour valider vos choix.
+
+# 5 Le fichier converti apparaitera dans le dossier où se trouve la vidéo d'origine.
--- video/video-converter.bat
+++ video/video-converter.bat
@@ -12,20 +12,23 @@ echo *  Si vous souhaitez juste convertir une video sans la couper,
 echo *  Passez les questions de debut et de fin de la video en 
 echo *  cliquant sur "Entrer"
 echo *  
-echo *  Bonne chance!
+echo *  Bonne conversion!
 echo *
 
-set /p inputFile=* Glissez et deposer la video a traiter ici
+set /p inputFile=* Glissez et deposer la video a traiter ici : 
+echo * 
 echo * La video a traiter sera %inputFile%
 echo *
 echo ************************************************************
 echo *
-set /p start=* Inscrire le debut de la video au format (HH:MM:SS)
+set /p start=* Inscrire le debut de la video au format (HH:MM:SS) : 
+echo * 
 echo * La video commencera a %start%
 echo * 
 echo ************************************************************
 echo *
-set /p end=* Inscrire la fin de la video au format (HH:MM:SS)
+set /p end=* Inscrire la fin de la video au format (HH:MM:SS) : 
+echo * 
 echo * La video terminera a %end%
 echo *
 echo ************************************************************
@@ -42,11 +45,11 @@ IF "%start%"=="" GOTO :parametresError
 IF "%end%"=="" GOTO :parametresError
 
 ffmpeg.exe -t %end% -i %inputFile% -ss %start% -vcodec libx264 -acodec copy -preset ultrafast -filter:v fps=fps=25 -b:v 800k %outputFile%
-EXIT
+GOTO :aurevoir
 
 :simpleConversion
 ffmpeg.exe -i %inputFile% -vcodec libx264 -acodec copy -preset ultrafast -filter:v fps=fps=25 -b 800k %outputFile%
-EXIT
+GOTO :aurevoir
 
 :parametresError
 echo *
@@ -55,6 +58,8 @@ echo * les commandes pour convertir ou couper une video.
 
 :aurevoir
 echo * 
+echo * La video convertie se trouve ici : "%outputFile%"
+echo *
 echo * Au revoir !
 echo * 
 pause
