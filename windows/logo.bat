35c7609d25cb706bf8dbc5fabc3411ecfba36afa
Antoine SMAGGHE <SMAGGHE@PROXIMUM.proximum.fr>
2020-10-28 18:41:21 +0100

    change logo.bat

--- windows/logo.bat
+++ windows/logo.bat
@@ -1,4 +1,4 @@
-\<style\>#logos{display:flex;flex-wrap:wrap;width:1113px;margin:auto;justify-content:center}.logo{width:228px;object-fit:scale-down;max-height:110px;padding:15px;display:flex;margin:auto 0}\<\/style\> > test.html
-\<section id='logos'\> >> test.html
-for %%a in (*.ink) do echo \<img src='%%a' class='logo'\> >> test.html
-\<\/section\> >> test.html
+@REM TODO
+@REM créer un script pour rendre des logos automatiquement dans une image.
+
+@REM Utiliser la methode OEF pour ecrire du html
