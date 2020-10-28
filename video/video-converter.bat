e5ad733b5db3d7d8fd6b7bac8bcac9235797064c
Antoine SMAGGHE <SMAGGHE@PROXIMUM.proximum.fr>
2020-10-28 11:05:46 +0100

    NEW: video assembler script and color syntax

--- video/video-converter.bat
+++ video/video-converter.bat
@@ -1,20 +1,21 @@
 @echo off
+chcp 65001
 echo ************************************************************
 echo *                                                          *
-echo *  Bienvenue dans l'utilitaire de decoupe video Proximum   *
+echo *  Bienvenue dans l'utilitaire de découpe video Proximum   *
 echo *                   version 2.0.2                          *
 echo *                                                          *
 echo ************************************************************
 echo *  
-echo *  Cliquez sur "Entrer" a chaque etapes pour valider vos choix.
-echo *  A tout moment tapez ctrl+c pour sortir du programme.
+echo *  Cliquez sur [1;92m"Entrer"[0m à chaque étapes pour valider vos choix.
+echo *  A tout moment tapez [91mctrl+c[0m pour sortir du programme.
 echo *  
 echo *  Que souhaitez vous faire ? 
 echo *  
-echo *  1. Convertir une video
-echo *  2. Couper et convertir une video
+echo *  [1;94m1. Convertir une vidéo[0m
+echo *  [1;94m2. Couper et convertir une vidéo[0m
 echo *  
-set /p choice=*  Taper le numero qui correspond a votre choix (1 ou 2), puis tapez sur "Entrer" : 
+set /p choice=*  Taper le numéro qui correspond à votre choix (1 ou 2), puis tapez sur "Entrer" : 
 cscript //nologo src/match.js "^[12]$" "%choice%"
 IF errorlevel 1 GOTO :parametresError
 
@@ -25,25 +26,25 @@ echo ************************************************************
 
 echo *
 set /p "inputFile=* Glissez et deposez la video a traiter ici, puis tapez sur 'Entrer' : "
-IF !inputFile!=="" GOTO :parametresError
-set outputFile=%inputFile:~0,-5%-convert.mp4"
+IF "%inputFile%"=="" GOTO :parametresError
+set outputFile=%inputFile:~0,-5%-convert.mp4
 
 echo * 
-echo * La video a traiter sera %inputFile%
+echo * La vidéo à traiter sera [94m%inputFile%[0m
 echo *
 echo ************************************************************
 IF "%choice%"=="1" GOTO :simpleConversion
 
 echo *
-set /p start=* Inscrire le debut de la video au format HH:MM:SS, puis tapez sur "Entrer" : 
+set /p start=* Inscrire le début de la vidéo au format HH:MM:SS, puis tapez sur "Entrer" : 
 echo * 
-echo * La video commencera a %start%
+echo * La vidéo commencera à [94m%start%[0m
 echo * 
 echo ************************************************************
 echo *
-set /p end=* Inscrire la fin de la video au format HH:MM:SS, puis tapez sur "Entrer" : 
+set /p end=* Inscrire la fin de la vidéo au format HH:MM:SS, puis tapez sur "Entrer" : 
 echo * 
-echo * La video terminera a %end%
+echo * La vidéo terminera à [94m%end%[0m
 echo *
 echo ************************************************************
 
@@ -51,22 +52,26 @@ IF "%start%"=="" IF "%end%"=="" GOTO :simpleConversion
 IF "%start%"=="" GOTO :parametresError
 IF "%end%"=="" GOTO :parametresError
 
+set startTime=%time%
 ffmpeg.exe -t %end% -i %inputFile% -ss %start% -vcodec libx264 -acodec copy -preset ultrafast -filter:v fps=fps=25 -b:v 1000k %outputFile%
+set endTime=%time%
 GOTO :aurevoirVideo
 
 :simpleConversion
+set startTime=%time%
 ffmpeg.exe -i %inputFile% -vcodec libx264 -acodec copy -preset ultrafast -filter:v fps=fps=25 -b:v 1000k %outputFile%
+set endTime=%time%
 GOTO :aurevoirVideo
 
 :parametresError
 echo *
-echo *  Erreur: veuillez entrer de nouveau les commandes pour convertir
-echo *  ou couper une video.
+echo *  [91mErreur: veuillez entrer de nouveau les commandes pour convertir[0m
+echo *  [91mou couper une vidéo.[0m
 GOTO :aurevoir
 
 :aurevoirVideo
 echo * 
-echo *  La video convertie se trouve ici : %outputFile%
+echo *  La vidéo convertie se trouve ici : [94m%outputFile%[0m
 
 :aurevoir
 echo *
