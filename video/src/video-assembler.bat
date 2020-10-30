2c52ffe1e38034f6299061a85593fadbcbb4a5fc
Antoine SMAGGHE <SMAGGHE@PROXIMUM.proximum.fr>
2020-10-30 17:29:44 +0100

    assembler video is working without whitespaces in path

--- video/src/video-assembler.bat
+++ video/src/video-assembler.bat
@@ -23,59 +23,68 @@ chcp 65001 > nul
 @REM echo              ``````````````````````````````````
 @REM echo *
 @REM echo *
-echo **************************************************************
-echo *                                                            *
-echo *  Bienvenue dans l'utilitaire d'assemblage vidéo Proximum   *
-echo *                   version 1.0.0                            *
-echo *                                                            *
-echo **************************************************************
+@REM echo **************************************************************
+@REM echo *                                                            *
+@REM echo *  Bienvenue dans l'utilitaire d'assemblage vidéo Proximum   *
+@REM echo *                   version 1.0.0                            *
+@REM echo *                                                            *
+@REM echo **************************************************************
 echo *
-echo *  [91m! Les vidéos à assembler doivent être du même format et du[0m
-echo *  [91mmême encodage. Si vous souhaitez assembler deux vidéos[0m
-echo *  [91mde différents formats / encodage, vous devez convertir[0m
-echo *  [91mles deux morceaux avec l'outil "vidéo-converter.bat" avant[0m
-echo *  [91mde les assembler ![0m
+echo *  [91m!!! Les vidéos à assembler doivent être du même format et du même encodage !!![0m
 echo *  
-echo *  Cliquez sur [92m'Entrer'[0m à chaque étapes pour valider vos choix.
-echo *  A tout moment tapez [91mctrl+c[0m pour sortir du programme.
-echo *
-pause
+echo *  [93mSi vous souhaitez assembler deux vidéos de différents[0m
+echo *  [93mformats / encodage, vous devez convertir les deux morceaux[0m
+echo *  [93mavant de les assembler ![0m
+echo *  
 echo *
 set /p "inputFile1=*  Glissez et déposez la première vidéo ici, puis tapez sur [92m'Entrer'[0m : "
 IF !inputFile1!=="" GOTO :parametresError
+set inputFile1=%inputFile1:"='%
+set inputFile1=%inputFile1:\=\\%
+set inputFile1=%inputFile1: =\ %
 
 echo * 
-echo * La première vidéo est [94m%inputFile1%[0m
+echo *  La première vidéo est [94m%inputFile1%[0m
 echo *
 echo ************************************************************
 
 echo *
 set /p "inputFile2=*  Glissez et déposez la seconde vidéo ici, puis tapez sur [92m'Entrer'[0m : "
 IF !inputFile2!=="" GOTO :parametresError
+set inputFile2=%inputFile2:"='%
+set inputFile2=%inputFile2:\=\\%
+set inputFile2=%inputFile2: =\ %
 
 echo * 
-echo * La seconde vidéo est [94m%inputFile2%[0m
+echo *  La seconde vidéo est [94m%inputFile2%[0m
 echo *
 echo ************************************************************
-set outputFile=%inputFile1:~0,-5%-join.mp4"
+set outputFile=%inputFile1:~0,-5%-join.mp4
 
 :joinvideo
-set startTime=%time%
-echo file %inputFile1:\=\\% > assembly.tmp
-echo file %inputFile2:\=\\% >> assembly.tmp
+cd src/
+echo file %inputFile1% > assembly.tmp
+echo file %inputFile2% >> assembly.tmp
+echo %outputFile%
 ffmpeg.exe -safe 0 -f concat -i assembly.tmp -c copy %outputFile%
 del assembly.tmp
-set endTime=%time%
+GOTO :aurevoirVideo
 
 :parametresError
 echo *
-echo *  Erreur: veuillez entrer de nouveau les commandes pour convertir
-echo *  ou couper une vidéo.
+echo *  [91mErreur: veuillez entrer de nouveau les commandes pour convertir[0m
+echo *  [91mou couper une vidéo.[0m
+echo * 
+set /p playAgain=*  Souhaitez vous revenir au menu ? (oui/non)
+IF "%playAgain%"=="oui" (cd ../ & video-converter.bat)
 GOTO :aurevoir
 
 :aurevoirVideo
 echo * 
-echo *  La vidéo convertie se trouve ici : [94m%outputFile%[0m
+echo *  La vidéo convertie se trouve ici : [94m%outputFile:\\=\%[0m
+echo *  
+set /p playAgain=*  Souhaitez vous effectuer une autre opération ? (oui/non)
+IF "%playAgain%"=="oui" (cd ../ & video-converter.bat)
 
 :aurevoir
 echo *
--- video/video-converter.bat
+++ video/video-converter.bat
@@ -2,22 +2,27 @@
 chcp 65001 > nul
 echo ************************************************************
 echo *                                                          *
-echo *  Bienvenue dans l'utilitaire de découpe video Proximum   *
+echo *        Bienvenue dans l'utilitaire video Proximum        *
 echo *                   version 2.1.0                          *
 echo *                                                          *
 echo ************************************************************
 echo *  
 echo *  Cliquez sur [1;92m"Entrer"[0m à chaque étapes pour valider vos choix.
 echo *  A tout moment tapez [91mctrl+c[0m pour sortir du programme.
+:choices
 echo *  
 echo *  Que souhaitez vous faire ? 
 echo *  
 echo *  [1;94m1. Convertir une vidéo[0m
 echo *  [1;94m2. Couper et convertir une vidéo[0m
+echo *  [1;94m3. Joindre deux vidéos[0m
+echo *  [1;94m0. Sortir[0m
 echo *  
-set /p choice=*  Taper le numéro qui correspond à votre choix (1 ou 2), puis tapez sur "Entrer" : 
-cscript //nologo src/match.js "^[12]$" "%choice%"
+set /p choice=*  Tapez le numéro qui correspond à votre choix (1, 2 ou 3), puis tapez sur [92m"Entrer"[0m : 
+cscript //nologo src/match.js "^[1230]$" "%choice%"
 IF errorlevel 1 GOTO :parametresError
+IF "%choice%"=="3" GOTO :videoassembler
+IF "%choice%"=="0" GOTO :aurevoir
 
 echo *  
 echo *  Bonne conversion!
@@ -25,7 +30,7 @@ echo *
 echo ************************************************************
 
 echo *
-set /p "inputFile=* Glissez et déposez la vidéo a traiter ici, puis tapez sur 'Entrer' : "
+set /p "inputFile=* Glissez et déposez la vidéo a traiter ici, puis tapez sur [92m'Entrer'[0m : "
 IF !inputFile!=="" GOTO :parametresError
 set outputFile=%inputFile:~0,-5%-convert.mp4"
 
@@ -36,13 +41,13 @@ echo ************************************************************
 IF "%choice%"=="1" GOTO :simpleConversion
 
 echo *
-set /p start=* Inscrire le début de la vidéo au format HH:MM:SS, puis tapez sur "Entrer" : 
+set /p start=* Inscrire le début de la vidéo au format HH:MM:SS, puis tapez sur [92m"Entrer"[0m : 
 echo * 
 echo * La vidéo commencera à [94m%start%[0m
 echo * 
 echo ************************************************************
 echo *
-set /p end=* Inscrire la fin de la vidéo au format HH:MM:SS, puis tapez sur "Entrer" : 
+set /p end=* Inscrire la fin de la vidéo au format HH:MM:SS, puis tapez sur [92m"Entrer"[0m : 
 echo * 
 echo * La vidéo terminera à [94m%end%[0m
 echo *
@@ -52,16 +57,13 @@ IF "%start%"=="" IF "%end%"=="" GOTO :simpleConversion
 IF "%start%"=="" GOTO :parametresError
 IF "%end%"=="" GOTO :parametresError
 
-set startTime=%time%
-@echo on
+cd src/
 ffmpeg.exe -t %end% -i %inputFile% -ss %start% -vcodec libx264 -acodec copy -preset ultrafast -filter:v fps=fps=25 -b:v 1000k %outputFile%
-set endTime=%time%
 GOTO :aurevoirVideo
 
 :simpleConversion
-set startTime=%time%
+cd src/
 ffmpeg.exe -i %inputFile% -vcodec libx264 -acodec copy -preset ultrafast -filter:v fps=fps=25 -b:v 1000k %outputFile%
-set endTime=%time%
 GOTO :aurevoirVideo
 
 :parametresError
@@ -72,11 +74,19 @@ GOTO :aurevoir
 
 :aurevoirVideo
 echo * 
+echo *  L'opération a durée %timeDelay%
 echo *  La vidéo convertie se trouve ici : [94m%outputFile%[0m
+echo *  
+set /p playAgain=*  Souhaitez vous effectuer une autre opération ? (oui/non)
+IF "%playAgain%"=="oui" GOTO :choices
 
 :aurevoir
 echo *
 echo *  Au revoir !
 echo * 
 pause
+EXIT
+
+:videoassembler
+src/video-assembler.bat
 EXIT
