37c0c203f9ae8eab84089c6adfbd1d97ac81de22
Antoine Smagghe <smagghe@proximumgroup.com>
2021-03-25 18:43:02 +0100

    Add watch for e2e tests

--- linux/e2e/watche2e.sh
+++ linux/e2e/watche2e.sh
@@ -1,6 +1,13 @@
 #!/bin/zsh
 # Watch e2e behat tests with tags @debug
 
+if ! which entr; then
+	echo 'ERROR: entr is required !'
+	echo 'You can get it with :\n'
+	echo '	sudo apt install entr\n'
+	exit
+fi
+
 projectFolder=$1
 
 cd "$projectFolder"
