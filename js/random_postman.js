56a08909200ba10592d5f25beb5e3e30be96f93f
Antoine Smagghe <smagghe@proximumgroup.com>
2021-09-17 18:54:24 +0200

    add random postman with weight

--- js/random_postman.js
+++ js/random_postman.js
@@ -1,12 +1,41 @@
 #!/bin/node
+
 const team = [
-	"Ricardo",
-	"Gregory",
-	"Emilie",
-	"Bertrand",
-	"Antoine"
+  { member: 'Emilie', weight: 75 },
+  { member: 'Gregory', weight: 75 },
+  { member: 'Ricardo', weight: 100 },
+  { member: 'Antoine', weight: 75 },
+  { member: 'Bertrand', weight: 75 },
 ];
 
-const randomIndex = Math.round(Math.random() * (team.length - 1));
+const teamTMA = [
+  { member: 'Emilie', weight: 75 },
+  { member: 'Gregory', weight: 75 },
+  { member: 'Ricardo', weight: 100 },
+];
 
-console.log("Le facteur est :", team[randomIndex]);
+function randomSample(samples) {
+  let sample =
+    Math.random() * samples.reduce((sum, { weight }) => sum + weight, 0);
+  const { member } = samples.find(({ weight }) => (sample -= weight) < 0);
+
+  return member;
+}
+
+function calculateProbability(teamArr) {
+  calculateArray = teamArr;
+  let counts = calculateArray.map((mem) => ({[mem.member]: 0 }));
+  console.log(counts);
+  Array.from({ length: 1000000 }, () => randomSample(teamArr)).forEach(
+    (member) => {
+		//TODO: bug ici ! 
+      console.log(counts.find((mem) => mem === member));
+    }
+  );
+  return counts;
+}
+
+console.log(calculateProbability(team));
+
+// console.log("\nFacteur : ", randomSample(team));
+// console.log("TMA Vimeet : ", randomSample(teamTMA), "\n");
